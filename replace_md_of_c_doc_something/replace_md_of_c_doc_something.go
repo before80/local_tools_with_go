b22964bf37c0e6b8e18aad1cc87debca3f2a3b60
zlongx <1251885400@qq.com>
2025-04-16 11:46:17 +0800

    更新

--- replace_md_of_c_doc_something/replace_md_of_c_doc_something.go
+++ replace_md_of_c_doc_something/replace_md_of_c_doc_something.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/eiannone/keyboard"
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -34,6 +35,7 @@ func replaceInFile(filePath string) (bool, error) {
 		pattern     *regexp.Regexp
 		replacement string
 	}{
+		{regexp.MustCompile("\n\n```\\s*?\n&zeroWidthSpace;"), "```\n&zeroWidthSpace;"},
 		{regexp.MustCompile("输出：\\s*?\n```\\s*?\n"), "输出：\n\n```txt\n"},
 		{regexp.MustCompile("```\\s*?\n#include"), "```c\n#include"},
 		{regexp.MustCompile("```\\s*?\ntypedef"), "```c\ntypedef"},
@@ -95,10 +97,24 @@ func replaceInFile(filePath string) (bool, error) {
 		{regexp.MustCompile("`\\*\\*%d\\*\\*`"), "`%d`"},
 		{regexp.MustCompile("`\\*\\*%f\\*\\*`"), "`%f`"},
 		{regexp.MustCompile("`\\*\\*%%\\*\\*`"), "`%%`"},
-		{regexp.MustCompile(`"\*\*\\t\*\*"`), "`\"\\t\"`"},
+		{regexp.MustCompile(`"\*\*\\f\*\*"`), "`\"\\f\"`"},
+		{regexp.MustCompile(`'\*\*\\f\*\*'`), "`'\\f'`"},
 		{regexp.MustCompile(`"\*\*\\n\*\*"`), "`\"\\n\"`"},
-		{regexp.MustCompile("`\\*\\*\\t\\*\\*`"), "`\\t`"},
+		{regexp.MustCompile(`'\*\*\\n\*\*'`), "`'\\n'`"},
+		{regexp.MustCompile(`"\*\*\\r\*\*"`), "`\"\\r\"`"},
+		{regexp.MustCompile(`'\*\*\\r\*\*'`), "`'\\r'`"},
+		{regexp.MustCompile(`"\*\*\\t\*\*"`), "`\"\\t\"`"},
+		{regexp.MustCompile(`'\*\*\\t\*\*'`), "`'\\t'`"},
+		{regexp.MustCompile(`"\*\*\\v\*\*"`), "`\"\\v\"`"},
+		{regexp.MustCompile(`'\*\*\\v\*\*'`), "`'\\v'`"},
+		{regexp.MustCompile("`\\*\\*\\f\\*\\*`"), "`\\f`"},
 		{regexp.MustCompile("`\\*\\*\\n\\*\\*`"), "`\\n`"},
+		{regexp.MustCompile("`\\*\\*\\r\\*\\*`"), "`\\r`"},
+		{regexp.MustCompile("`\\*\\*\\t\\*\\*`"), "`\\t`"},
+		{regexp.MustCompile("`\\*\\*\\v\\*\\*`"), "`\\v`"},
+		{regexp.MustCompile("`\\*\\*INF\\*\\*`"), "`INF`"},
+		{regexp.MustCompile("`\\*\\*INFINITY\\*\\*`"), "`INFINITY`"},
+		{regexp.MustCompile("`\\*\\*NAN\\*\\*`"), "`NAN`"},
 	}
 
 	modified := false
@@ -127,15 +143,23 @@ func main() {
 		return
 	}
 	dir := os.Args[1]
+
+	// 初始化键盘监听
+	if err := keyboard.Open(); err != nil {
+		log.Fatalf("无法初始化键盘监听: %v", err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	files, err := findMarkdownFiles(dir)
+	if err != nil {
+		fmt.Printf("查找 .md 文件时出错: %v\n", err)
+		return
+	}
 	for {
-		files, err := findMarkdownFiles(dir)
-		if err != nil {
-			fmt.Printf("查找 .md 文件时出错: %v\n", err)
-			return
-		}
 		replacedCount := 0
 		for _, file := range files {
-			fmt.Println("file=", file)
 			replaced, err := replaceInFile(file)
 			if err != nil {
 				fmt.Printf("处理文件 %s 时出错: %v\n", file, err)
@@ -146,19 +170,37 @@ func main() {
 			}
 		}
 		fmt.Printf("本次替换了 %d 个文件中的内容。\n", replacedCount)
-		fmt.Println("是否再次执行？输入 1 再次执行，输入 2 退出：")
+		//fmt.Println("是否再次执行？输入 1 再次执行，输入 2 退出：")
+		//labelForJudge:
+		//	reader := bufio.NewReader(os.Stdin)
+		//	input, err := reader.ReadString('\n')
+		//	if err != nil {
+		//		fmt.Printf("读取输入时出错: %v\n", err)
+		//		return
+		//	}
+		//	input = strings.TrimSpace(input)
+		//	if input == "2" {
+		//		break
+		//	} else if input != "1" {
+		//		fmt.Println("输入无效，请输入 1 或 2。")
+		//		goto labelForJudge
+		//	}
 	labelForJudge:
-		reader := bufio.NewReader(os.Stdin)
-		input, err := reader.ReadString('\n')
+		fmt.Println("按 CTRL+2 继续，CTRL+3 退出")
+		// 捕获按键事件
+		_, key, err := keyboard.GetKey()
 		if err != nil {
-			fmt.Printf("读取输入时出错: %v\n", err)
-			return
+			log.Fatalf("读取键盘输入时出错: %v", err)
 		}
-		input = strings.TrimSpace(input)
-		if input == "2" {
-			break
-		} else if input != "1" {
-			fmt.Println("输入无效，请输入 1 或 2。")
+
+		// 检查是否按下了 Ctrl 键组合
+		if key == keyboard.KeyCtrl2 { // Ctrl + 2
+			continue
+		} else if key == keyboard.KeyCtrl3 { // Ctrl + 3
+			fmt.Println("\n退出程序...")
+			break // 跳出循环
+		} else {
+			fmt.Println("输入无效，请按下 Ctrl + 2 继续，或者按下 Ctrl + 3 退出。")
 			goto labelForJudge
 		}
 	}
